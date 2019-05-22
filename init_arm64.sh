e30aa734574fab8c222ac85bfbd310a57a50b307
ngot <zhuanghengfei@gmail.com>
2019-05-23 00:01:09 +1000

    f

--- init_arm64.sh
+++ init_arm64.sh
@@ -11,7 +11,7 @@ cp -f /etc/nsswitch.conf ./arm64_root_fs/etc/nsswitch.conf
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 
 # https://wiki.gentoo.org/wiki/Embedded_Handbook/General/Compiling_with_qemu_user_chroot
-echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64-static:' > /proc/sys/fs/binfmt_misc/register
+echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:' > /proc/sys/fs/binfmt_misc/register
 
 mount -t proc proc     ./arm64_root_fs/proc/
 mount -t sysfs sys     ./arm64_root_fs/sys/
