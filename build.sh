6245fe21476371f08f8c664366997e1c498ad261
ngot <zhuanghengfei@gmail.com>
2019-05-22 22:40:16 +1000

    f

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ set -ev
 DIR=`pwd`;sudo docker run --privileged=true -it -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
         cd /home;
         sh init_arm64.sh;
-        cp -f ./ci/fibjs ./arm64_root_fs/bin/fibjs;
-        chroot ./arm64_root_fs fibjs -v"
+        cp -f ./ci/fibjs64 ./arm64_root_fs/bin/fibjs;
+        cp -f ./ci/fibjs ./arm_root_fs/bin/fibjs;
+        ls -la ./arm64_root_fs/bin/fibjs;
+        ls -la ./arm_root_fs/bin/fibjs;
+        chroot ./arm64_root_fs fibjs -v
+        chroot ./arm_root_fs fibjs -v"
 
 exit 0;
